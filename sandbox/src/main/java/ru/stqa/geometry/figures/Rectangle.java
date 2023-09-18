b931271424764b627a6b5b1869021bc8399321ab
milad555 <gebriel007@gmail.com>
2023-09-17 17:51:50 -0700

    rewrite Triangle to use Record

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,6 +1,10 @@
 package ru.stqa.geometry.figures;
 
-public class Rectangle {
+public record Rectangle(
+
+        double a,
+        double b
+) {
     public static void printRectangleArea(double a, double b) {
         var text = String.format("Площадь квадрата со стороной %f %f = %f", a, b, rectangleArea(a,b));
         System.out.println(text);
