d88cfa546c011a9205db45b5e401182b5a2c39d3
milad555 <gebriel007@gmail.com>
2023-09-17 17:55:57 -0700

    Изменил класс Triangle чтоб использовать record

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,31 +1,23 @@
 package ru.stqa.geometry.figures;
 
-public class Triangle {
-    private double side1;
-    private double side2;
-    private double side3;
+public record Triangle( double a, double b, double c) {
 
-    public Triangle (double side1, double side2, double side3){
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
-    }
 
     public static void printTriangleArea(Triangle a){
-        String text = String.format("Площадь треугольника со сторонами %f, %f , %f  = %f ",a.side1, a.side2, a.side3, a.tArea());
+        String text = String.format("Площадь треугольника со сторонами %f, %f , %f  = %f ",a.a, a.b, a.c, a.tArea());
         System.out.println(text);
     }
 
     public double tArea(){
-        double p = 0.5 * (this.side1 + this.side2 + this.side3);
-        return Math.sqrt(p * ((p - this.side1) * (p - this.side2) * (p - this.side3)));
+        double p = 0.5 * (this.a + this.b + this.c);
+        return Math.sqrt(p * ((p - this.a) * (p - this.b) * (p - this.c)));
     }
 
     public double tPerimeter(){
-        return this.side1 + this.side2 + this.side3;
+        return this.a + this.b + this.c;
     }
     public static void printTrianglePerimeter(Triangle a){
-        String text = String.format("Площадь треугольника со сторонами %f, %f , %f  = %f ",a.side1, a.side2, a.side3, a.tPerimeter());
+        String text = String.format("Площадь треугольника со сторонами %f, %f , %f  = %f ",a.a, a.b, a.c, a.tPerimeter());
         System.out.println(text);
     }
 }
