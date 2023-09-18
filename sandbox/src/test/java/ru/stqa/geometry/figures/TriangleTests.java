536f383444ac98f4bbd401f1446732242121735d
milad555 <gebriel007@gmail.com>
2023-09-17 17:27:40 -0700

    ДЗ №2

--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -3,13 +3,12 @@ package ru.stqa.geometry.figures;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static ru.stqa.geometry.figures.Triangle.tArea;
-
 public class TriangleTests {
     @Test
     void canCalcArea(){
         double extRes = 9.922;
-        String resultFormatted = String.format("%.3f",tArea(4,5,6));
+        var s = new Triangle(4,5,6);
+        String resultFormatted = String.format("%.3f",s.tArea());
         double parsedDouble = Double.parseDouble(resultFormatted);
         Assertions.assertEquals(extRes, parsedDouble);
 
