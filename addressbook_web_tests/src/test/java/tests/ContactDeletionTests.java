18d32f799b7b5b52656421fa85147c62923f2703
milad555 <gebriel007@gmail.com>
2023-10-10 17:58:34 -0700

    лекция 4.7

--- addressbook_web_tests/src/test/java/tests/ContactDeletionTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactDeletionTests.java
@@ -17,8 +17,7 @@ public class ContactDeletionTests extends TestBase {
     }
 
     @Test
-    //deletes contacts one by one
-    public void canDeleteAllContact(){
+    public void canDeleteAllContactAtOnce(){
         if (app.groups().getCount() == 0){
             app.contacts().createContact(new ContactData("John", "Doe", "1st Street, Irvine, CA", "123-456", "test@test.com"));
         }
